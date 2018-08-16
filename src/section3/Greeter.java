7fbb629ff2d0fbbbc78a3ce653775ed14d57bb06
justinmastro <league@iMac-2.attlocal.net>
2018-08-16 11:59:01 -0700

    Lots of fun

--- src/section3/Greeter.java
+++ src/section3/Greeter.java
@@ -3,10 +3,12 @@ package section3;
 import javax.swing.JOptionPane;
 
 public class Greeter {
+	private static final String Speak = null;
+
 	public static void main(String[] args) {
 		String name= JOptionPane.showInputDialog("What is your name");
 		JOptionPane.showMessageDialog(null,"Hello "+name);
 		JOptionPane.showMessageDialog(null,"What is your favorite book");
-		
+JOptionPane.showMessageDialog(null, );		
 	}
 }
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,14 +1,23 @@
 package section4;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
+		int random=new Random().nextInt(20);
 		
-		// 2.  Ask the user a question 
+		// 2.  Ask the user a question
+		String s=JOptionPane.showInputDialog("What is my name"); of(s.equals("Justin")){
+		}
 		
 		// 3.  Use an if statement to check if their answer is correct
+		
+		
 		
 		// 4.  if the user's answer is correct
 		
@@ -17,6 +26,11 @@ public class QuizGame {
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
+		
+	}
+
+	private static void of(boolean equals) {
+		// TODO Auto-generated method stub
 		
 	}
 }
